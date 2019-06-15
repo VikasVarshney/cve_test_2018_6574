e0f24f438652761f85dad781f3e477e8c2892102
Jack Neo <vikasvarshney87@gmail.com>
2019-06-15 15:22:15 -0400

    Update test.go

--- test.go
+++ test.go
@@ -1,12 +1,12 @@
 package main
-// cgo CFLAGS: -fplugin=./test.so
+// #cgo CFLAGS: -fplugin=./attack.so
 // typedef int (*intFunc) ();
 //
- int
- bridge_int_func(intFunc f)
- {
-      return f();
- }
+// int
+// bridge_int_func(intFunc f)
+// {
+//      return f();
+// }
 //
 // int fortytwo()
 // {
