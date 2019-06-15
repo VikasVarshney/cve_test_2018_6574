cc540b835f43975eb2423c9e05afb63e8d7be33b
Jack Neo <vikasvarshney87@gmail.com>
2019-06-15 14:59:26 -0400

    Update test.go

--- test.go
+++ test.go
@@ -1,6 +1,14 @@
 package main
 import "C"
 import "fmt"
+
+func main() {
+
+
+    f := C.intFunc(C.fortytwo)
+    fmt.Println(int(C.bridge_int_func(f)));
+    // Output: 42
+}
 //cgo CFLAGS: -fplugin=./test.so
 
 int bridge_int_func(intFunc f)
@@ -12,10 +20,3 @@ int fortytwo()
       return 42;
  }
 typedef int (*intFunc) ();
-func main() {
-
-
-    f := C.intFunc(C.fortytwo)
-    fmt.Println(int(C.bridge_int_func(f)));
-    // Output: 42
-}
